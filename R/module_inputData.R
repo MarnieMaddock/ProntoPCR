37a4ef77975dd5fe9a5cd4191745c1ffb47acc88
MarnieMaddock <mlm715@uowmail.edu.au>
2024-09-08 15:40:49 +1000

    test

--- R/module_inputData.R
+++ R/module_inputData.R
@@ -34,7 +34,7 @@ inputFileUI <- function(id) {
 }
 
 
-downloadExampleData <- function(id, dataset_path = ystem.file("www/exampledata.csv", package = "ProntoPCR")) {
+downloadExampleData <- function(id, dataset_path = system.file("inst/www/exampledata.csv", package = "ProntoPCR")) {
   moduleServer(
     id,
     function(input, output, session) {
