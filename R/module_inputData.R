2fcddfc8f970f125ae0e0fd6285dc1706645292b
MarnieMaddock <mlm715@uowmail.edu.au>
2024-09-08 16:22:35 +1000

    test

--- R/module_inputData.R
+++ R/module_inputData.R
@@ -34,21 +34,40 @@ inputFileUI <- function(id) {
 }
 
 
-downloadExampleData <- function(id, dataset_path = system.file("www", "exampledata.csv", package = "ProntoPCR")) {
+# downloadExampleData <- function(id, dataset_path = system.file("www", "exampledata.csv", package = "ProntoPCR")) {
+#   moduleServer(
+#     id,
+#     function(input, output, session) {
+#       output$download_example  <- downloadHandler(
+#         filename = function() {
+#           "exampledata.csv"
+#         },
+#         content = function(file) {
+#           file.copy(dataset_path, file)
+#         }
+#       )
+#     }
+#   )
+# }
+print(system.file("www", "exampledata.csv", package = "ProntoPCR"))
+
+downloadExampleData <- function(id) {
   moduleServer(
     id,
     function(input, output, session) {
-      output$download_example  <- downloadHandler(
+      output$download_example <- downloadHandler(
         filename = function() {
           "exampledata.csv"
         },
         content = function(file) {
+          dataset_path <- system.file("www", "exampledata.csv", package = "ProntoPCR")
           file.copy(dataset_path, file)
         }
       )
     }
   )
 }
+
 
 inputDataUI <- function(id) {
   ns <- NS(id)
