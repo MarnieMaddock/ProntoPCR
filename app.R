eb3cecb6cafda5621ce77af4368817c64a7f63b1
Marnie Maddock <marnie.maddock@hotmail.com>
2025-02-16 18:18:14 +1100

    shinyapp download req

--- app.R
+++ app.R
@@ -38,6 +38,11 @@ library(kableExtra)
 # Dynamically source all module files in the R/ directory
 module_files <- list.files("R", pattern = "^module_.*\\.R$", full.names = TRUE)
 sapply(module_files, source, local = TRUE)
+
+if (!requireNamespace("ProntoPCR", quietly = TRUE)) {
+  devtools::install_github("MarnieMaddock/ProntoPCR")
+}
+
 # Run the app
 ProntoPCR()
 
